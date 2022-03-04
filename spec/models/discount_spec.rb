41494ffe6aeef794d2d5ffe1c81d5ecfc6b26950
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-04 06:35:43 -0500

    Test: add test for merchant/discount relationship

---
+++ spec/models/discount_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Discount do
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :quantity_thresh }
+    it { should validate_presence_of :percentage }
+
+  end
+  describe "relationships" do
+    it { should belong_to :merchant }
+  end
+end
