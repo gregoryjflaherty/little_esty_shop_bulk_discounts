f31c0bc6919b9447a97fbc26c272fede6537e74d
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-04 07:11:20 -0500

    Refactor: edit controller to use before action

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,10 +1,22 @@
 class DiscountsController < ApplicationController
+  before_action :set_merchant, only: [:index, :show]
+  before_action :set_discount, only: [:show]
+
   def index
-    @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.discounts
   end
 
   def show
+
+  end
+
+  private
+
+  def set_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def set_discount
     @discount = Discount.find(params[:id])
   end
 end
