893e567121a0680aef7bb18050e65bbaf58912de
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-08 11:59:22 -0500

    Feat: add instance methods to invoice model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -4,4 +4,7 @@ class Discount < ApplicationRecord
                         :quantity_threshold,
                         :percentage
   belongs_to :merchant
+  has_many :items, through: :merchant
+  has_many :invoice_items, through: :items
+  has_many :invoices, through: :invoice_items 
 end
